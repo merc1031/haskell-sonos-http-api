56926a1fe4c454c964948ac5808048789e644363
Leonidas Loucas <lloucas@imvu.com>
2015-11-12 01:35:56 -0800

    cleanup some code. refactor for reuse

--- src/Sonos/Lib.hs
+++ src/Sonos/Lib.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 module Sonos.Lib where
 
 import System.FilePath.Glob
@@ -12,33 +14,34 @@ import Text.XML
 import Text.XML.Cursor
 import Control.Concurrent.STM
 
-import Data.String (fromString)
 import Sonos.Types
-import Sonos.Util (findCoordinatorForIp)
 
-import Control.Monad (forever)
+import Data.String                          (fromString, IsString)
+import Sonos.Util                           (findCoordinatorForIp)
+import Control.Monad                        (forever)
+import Control.Concurrent                   (threadDelay)
+import Control.Lens                         ((^?), (.~), (&))
+import Data.Monoid                          ((<>))
+import Data.Char                            (toLower)
 
-import Control.Concurrent (threadDelay)
+import Sonos.Discover                       (getTopology)
 
-import Control.Lens ((^?), (.~), (&))
-import Data.Monoid ((<>))
-import Data.Char (toLower)
-import Sonos.Discover (getTopology)
---import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Char8 as BSC
-import qualified Web.Spock as WS
-import qualified Data.Map.Strict as M
-import qualified Text.XML.Light.Extractors as E
-import qualified Data.Text as T
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.ByteString.Char8      as BSC
+import qualified Web.Spock                  as WS
+import qualified Data.Map.Strict            as M
+import qualified Text.XML.Light.Extractors  as E
+import qualified Data.Text                  as T
 
 findMatchingArtistGlob :: CliArguments
                        -> String
                        -> IO [[String]]
 findMatchingArtistGlob args like = do
     putStrLn $ "What did we hear: " ++ like
-    res <- globDirWith  (matchDefault { ignoreCase = True } ) [(compile $ "**/" ++ like ++ "/**/*.flac")] (dir args)
-    putStrLn $ "Results were" ++ (show $ fst res)
+    res <- globDirWith (matchDefault { ignoreCase = True } )
+           [compile $ "**/" ++ like ++ "/**/*.flac"]
+           (dir args)
+    putStrLn $ "Results were" ++ show (fst res)
     return $ fst res
 
 
@@ -47,42 +50,129 @@ findMatchingGlob :: CliArguments
                  -> IO [[String]]
 findMatchingGlob args like = do
     putStrLn $ "What did we hear: " ++ like
-    res <- globDirWith  (matchDefault { ignoreCase = True } ) [(compile $ "**/*" ++ like ++ "*.flac")] (dir args)
-    putStrLn $ "Results were" ++ (show $ fst res)
+    res <- globDirWith (matchDefault { ignoreCase = True } )
+                       [compile $ "**/*" ++ like ++ "*.flac"]
+                       (dir args)
+    putStrLn $ "Results were" ++ show (fst res)
     return $ fst res
 
-envelopePreamble = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body>"
-envelopePostamble = "</s:Body><s:Envelope>"
+envelope :: String
+         -> String
+envelope content =
+    wrap (Name "Envelope"
+               (Just "http://schemas.xmlsoap.org/soap/envelope/")
+               (Just "s")
+         )
+         (Just "http://schemas.xmlsoap.org/soap/encoding/")
+         $ wrap (Name "Body" Nothing (Just "s"))
+                Nothing
+                content
 
-envelope cmd = envelopePreamble ++ cmd ++ envelopePostamble
+wrap :: Name
+     -> Maybe String
+     -> String
+     -> String
+wrap (Name {..}) ec content =
+    let (pfx,pfxs) = case namePrefix of
+            Just p -> (T.unpack p ++ ":", ":" ++ T.unpack p)
+            Nothing -> ("","")
+        ns = case nameNamespace of
+            Just n -> " xmlns" ++ pfxs ++ "=\"" ++ T.unpack n ++ "\""
+            Nothing -> ""
+        ec' = case ec of
+            Just e -> " " ++ pfx ++ "encodingStyle=\"" ++ e ++ "\""
+            Nothing -> ""
+    in "<"
+    ++ pfx
+    ++ T.unpack nameLocalName
+    ++ ns
+    ++ ec'
+    ++ ">"
+    ++ content
+    ++ "</"
+    ++ pfx
+    ++ T.unpack nameLocalName
+    ++ ">"
 
+avTransportAction :: String
 avTransportAction = "urn:schemas-upnp-org:service:AVTransport:1"
+
+avTransportNS :: String
 avTransportNS = "xmlns:u=\"" ++ avTransportAction ++ "\""
 
-addURIToQueueTemplate uri meta first next =
-    let addUri cts = "<u:AddURIToQueue " ++ avTransportNS ++ "><InstanceID>0</InstanceID>" ++ cts ++ "</u:AddURIToQueue>"
-        enqueuedURI = "<EnqueuedURI>" ++ uri ++ "</EnqueuedURI>"
-        enqueuedURIMetadata = "<EnqueuedURIMetaData>" ++ meta ++ "</EnqueuedURIMetaData>"
-        desiredFTN = "<DesiredFirstTrackNumberEnqueued>" ++ show first ++ "</DesiredFirstTrackNumberEnqueued>"
-        enqueueAsNext = "<EnqueueAsNext>" ++ show next ++ "</EnqueueAsNext>"
-    in addUri (enqueuedURI ++ enqueuedURIMetadata ++ desiredFTN ++ enqueueAsNext)
 
-playTemplate = "<u:Play " ++ avTransportNS ++ "><InstanceID>0</InstanceID><Speed>1</Speed></u:Play>"
+addURIToQueueTemplate_ :: String
+                       -> String
+                       -> Int
+                       -> Int
+                       -> String
+addURIToQueueTemplate_ uri meta first next =
+    avTransportTemplate "AddURIToQueue"
+                        [ ("InstanceID", "0")
+                        , ("EnqueuedURI", uri)
+                        , ("EnqueuedURIMetaData", meta)
+                        , ("DesiredFirstTrackNumberEnqueued", show first)
+                        , ("EnqueueAsNext", show next)
+                        ]
 
-setAVTransportURITemplate uri meta =
-    let avTransport cts = "<u:SetAVTransportURI " ++ avTransportNS ++ "><InstanceID>0</InstanceID>" ++ cts ++ "</u:SetAVTransportURI>"
-        currentURI = "<CurrentURI>" ++ uri ++ "</CurrentURI>"
-        currentURIMetadata = "<CurrentURIMetaData>" ++ meta ++ "</CurrentURIMetaData>"
-    in avTransport (currentURI ++ currentURIMetadata)
+playTemplate_ :: String
+playTemplate_ =
+    avTransportTemplate "Play"
+                        [ ("InstanceID", "0")
+                        , ("Speed", "1")
+                        ]
 
-becomeCoordinatorOfStandaloneGroup md =
-    let avTransport cts = "<u:BecomeCoordinatorOfStandaloneGroup " ++ avTransportNS ++ ">" ++ cts ++ "</u:BecomeCoordinatorOfStandaloneGroup>"
-        md' = foldl1 (++) $ map mk md
-        mk (w, i) = "<" ++ w ++ ">" ++ i ++ "</" ++ w ++ ">"
-    in avTransport (md')
+setAVTransportURITemplate_ :: String
+                           -> String
+                           -> String
+setAVTransportURITemplate_ uri meta =
+    avTransportTemplate "SetAVTransportURI"
+                        [ ("InstanceID", "0")
+                        , ("CurrentURI", uri)
+                        , ("CurrentURIMetaData", meta)
+                        ]
 
-seekTrackTemplate track = "<u:Seek " ++ avTransportNS ++ "><InstanceID>0</InstanceID><Unit>TRACK_NR</Unit><Target>" ++ show track ++ "</Target></u:Seek>"
+becomeCoordinatorOfStandaloneGroup_ :: String
+becomeCoordinatorOfStandaloneGroup_ =
+    avTransportTemplate "BecomeCoordinatorOfStandaloneGroup"
+                        [ ("InstanceID", "0")
+                        ]
 
+seekTrackTemplate_ :: Int
+                   -> String
+seekTrackTemplate_ track =
+    avTransportTemplate "Seek"
+                        [ ("InstanceID", "0")
+                        , ("Unit", "TRACK_NR")
+                        , ("Target", show track)
+                        ]
+
+wrap_ :: (String, String)
+      -> String
+wrap_ (w, i) = wrap (fromString w)
+                    Nothing
+                    i
+
+avTransportTemplate :: String
+                    -> [(String, String)]
+                    -> String
+avTransportTemplate action md =
+    let avTransport cts = "<u:"
+                       ++ action
+                       ++ " "
+                       ++ avTransportNS
+                       ++ ">"
+                       ++ cts
+                       ++ "</u:"
+                       ++ action
+                       ++ ">"
+        md' = foldl1 (++) $ map wrap_ md
+    in avTransport md'
+
+soapAction :: String
+           -> String
+           -> String
+           -> IO (Maybe BSL.ByteString)
 soapAction host action msg = do
     let opts = defaults & header "Host" .~ [BSC.pack host]
                         & header "User-Agent" .~ ["Haskell post"]
@@ -93,7 +183,9 @@ soapAction host action msg = do
 
     print opts
     print msg
-    resp <- postWith opts ("http://" ++ host ++ ep) (BSC.pack $ envelope msg)
+    resp <- postWith opts
+                     ("http://" ++ host ++ ep)
+                     (BSC.pack $ envelope msg)
 
     print $ resp ^? responseBody
     print $ resp ^? responseStatus
@@ -122,18 +214,18 @@ groupRoom zps args a b = do
         coordB = findCoordinatorForIp (zpLocation b) zps
         addr = let l = zpLocation coordA
                in lUrl l ++ ":" ++ lPort l
-    let avMessage = setAVTransportURITemplate ("x-rincon:"  ++ (zpUUID coordB)) ""
+    let avMessage = setAVTransportURITemplate_ ("x-rincon:" ++ zpUUID coordB) ""
     soapAction addr "SetAVTransportURI" avMessage
     return ()
 
 ungroupRoom :: [ZonePlayer]
-          -> CliArguments
-          -> ZonePlayer
-          -> IO ()
+            -> CliArguments
+            -> ZonePlayer
+            -> IO ()
 ungroupRoom zps args room = do
     let addr = let l = zpLocation room
                in lUrl l ++ ":" ++ lPort l
-    let avMessage = becomeCoordinatorOfStandaloneGroup [("InstanceID", show 0)]
+    let avMessage = becomeCoordinatorOfStandaloneGroup_
     soapAction addr "BecomeCoordinatorOfStandaloneGroup" avMessage
     return ()
 
@@ -150,13 +242,13 @@ queueAndPlayTrackLike zps args host like = do
         addr = let l = zpLocation coord
                in lUrl l ++ ":" ++ lPort l
 
-    putStrLn $ ("Coord was: " ++ show  coord)
-    uuid <- fetchUUID (addr)
-    let avMessage = setAVTransportURITemplate ("x-rincon-queue:" ++ (T.unpack uuid) ++ "#0") ""
+    putStrLn ("Coord was: " ++ show  coord)
+    uuid <- fetchUUID addr
+    let avMessage = setAVTransportURITemplate_ ("x-rincon-queue:" ++ T.unpack uuid ++ "#0") ""
 
     soapAction addr "SetAVTransportURI" avMessage
-    soapAction addr "Seek" (seekTrackTemplate trackNo)
-    soapAction addr "Play" playTemplate
+    soapAction addr "Seek" (seekTrackTemplate_ trackNo)
+    soapAction addr "Play" playTemplate_
 
 
     return ()
@@ -173,23 +265,34 @@ queueTrackLike zps args host like = do
     putStrLn $ "First track is:" ++ firstTrack
 
 
-    let firstTrackE = urlEncode $ replace ".flac" ".mp3" $ replace (dir args) "" $ firstTrack
-        soapMessage = addURIToQueueTemplate ("x-file-cifs://asgard/mp3Music/" ++ firstTrackE) "" 0 0
+    let firstTrackE = urlEncode
+                    $ replace ".flac" ".mp3"
+                    $ replace (dir args) "" firstTrack
+        soapMessage = addURIToQueueTemplate_ ("x-file-cifs://asgard/mp3Music/" ++ firstTrackE)
+                                             ""
+                                             0
+                                             0
         addr = let l = zpLocation coord
                in lUrl l ++ ":" ++ lPort l
 
-    putStrLn $ ("Coord was: " ++ show  coord)
+    putStrLn ("Coord was: " ++ show coord)
     Just queuedBody <- soapAction addr "AddURIToQueue" soapMessage
     return queuedBody
 
+fetchUUID :: String
+          -> IO T.Text
 fetchUUID host = do
     Just body <- getXMLDescription host
     return $ getUUID body
 
+getXMLDescription :: String
+                  -> IO (Maybe BSL.ByteString)
 getXMLDescription host = do
     res <- get $ "http://" ++ host ++ "/xml/device_description.xml"
     return $ res ^? responseBody
 
+getUUID :: BSL.ByteString
+        -> T.Text
 getUUID body =
     let cursor = fromDocument $ parseLBS_ def body
         ns = "{urn:schemas-upnp-org:device-1-0}"
